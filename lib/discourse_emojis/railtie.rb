09ae731a51aca0ac165e8e9cf835776acdafc4ac
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-02-10 16:02:20 +0100

    simpler log

--- lib/discourse_emojis/railtie.rb
+++ lib/discourse_emojis/railtie.rb
@@ -3,18 +3,13 @@
 module DiscourseEmojis
   class Railtie < ::Rails::Railtie
     initializer "discourse_emojis.configure_application" do |app|
-      symlink_results = []
-
       emoji_dir = File.join(app.config.root, "public/images/emoji")
+
       if !Dir.exist?(emoji_dir) || File.realpath(emoji_dir) != DiscourseEmojis.path_for_emojis
         FileUtils.rm_rf(emoji_dir) if Dir.exist?(emoji_dir)
         Discourse::Utils.atomic_ln_s(DiscourseEmojis.path_for_emojis, emoji_dir)
-        symlink_results << "emoji directory: #{DiscourseEmojis.path_for_emojis} -> #{emoji_dir}"
+        STDERR.puts "\nCreated emoji symlink: #{DiscourseEmojis.path_for_emojis} -> #{emoji_dir}\n\n"
       end
-
-      STDERR.puts "\nCreated emoji symlinks:"
-      STDERR.puts symlink_results.map { |r| "  #{r}" }.join("\n")
-      STDERR.puts "\n"
     end
   end
 end
