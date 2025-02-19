5a104a38b4175796087d5a71381b264be04f3fac
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-02-19 18:13:12 +0100

    symlink into railtie

--- lib/discourse_emojis/railtie.rb
+++ lib/discourse_emojis/railtie.rb
@@ -8,6 +8,22 @@ module DiscourseEmojis
       if !Dir.exist?(emoji_dir) || File.realpath(emoji_dir) != DiscourseEmojis.path_for_emojis
         FileUtils.rm_rf(emoji_dir) if Dir.exist?(emoji_dir)
         Discourse::Utils.atomic_ln_s(DiscourseEmojis.path_for_emojis, emoji_dir)
+
+        DEPRECATED_SYMLINKS = {
+          "apple" => "unicode",
+          "facebook_messenger" => "unicode",
+          "google" => "noto",
+          "google_classic" => "noto",
+          "win10" => "dist/emoji/fluentui",
+        }
+
+        DEPRECATED_SYMLINKS.each do |target, source|
+          Discourse::Utils.atomic_ln_s(
+            File.join(DiscourseEmojis.path_for_emojis, source),
+            File.join(emoji_dir, target),
+          )
+        end
+
         STDERR.puts "\nCreated emoji symlink: #{DiscourseEmojis.path_for_emojis} -> #{emoji_dir}\n\n"
       end
     end
