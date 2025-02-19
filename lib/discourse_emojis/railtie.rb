e4402a0770159657eefb35e2968f93521aeacd9a
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-02-19 20:11:04 +0100

    fix symlinks

--- lib/discourse_emojis/railtie.rb
+++ lib/discourse_emojis/railtie.rb
@@ -5,27 +5,27 @@ module DiscourseEmojis
     initializer "discourse_emojis.configure_application" do |app|
       emoji_dir = File.join(app.config.root, "public/images/emoji")
 
-      if !Dir.exist?(emoji_dir) || File.realpath(emoji_dir) != DiscourseEmojis.path_for_emojis
-        FileUtils.rm_rf(emoji_dir) if Dir.exist?(emoji_dir)
-        Discourse::Utils.atomic_ln_s(DiscourseEmojis.path_for_emojis, emoji_dir)
+      FileUtils.rm_rf(emoji_dir)
 
-        DEPRECATED_SYMLINKS = {
-          "apple" => "unicode",
-          "facebook_messenger" => "unicode",
-          "google" => "noto",
-          "google_classic" => "noto",
-          "win10" => "dist/emoji/fluentui",
-        }
+      Discourse::Utils.atomic_ln_s(DiscourseEmojis.path_for_emojis, emoji_dir)
 
-        DEPRECATED_SYMLINKS.each do |target, source|
-          Discourse::Utils.atomic_ln_s(
-            File.join(DiscourseEmojis.path_for_emojis, source),
-            File.join(emoji_dir, target),
-          )
-        end
+      # All the sets are defined in core discourse at app/models/emoji_set_site_setting.rb
+      DEPRECATED_SYMLINKS = {
+        "apple" => "unicode",
+        "facebook_messenger" => "unicode",
+        "google" => "noto",
+        "google_classic" => "noto",
+        "win10" => "fluentui",
+      }
 
-        STDERR.puts "\nCreated emoji symlink: #{DiscourseEmojis.path_for_emojis} -> #{emoji_dir}\n\n"
+      DEPRECATED_SYMLINKS.each do |target, source|
+        Discourse::Utils.atomic_ln_s(
+          File.join(DiscourseEmojis.path_for_emojis, source),
+          File.join(emoji_dir, target),
+        )
       end
+
+      STDERR.puts "\nCreated emoji symlink: #{DiscourseEmojis.path_for_emojis} -> #{emoji_dir}\n\n"
     end
   end
 end
